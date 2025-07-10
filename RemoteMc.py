f5fdac6817abc6e44704712c10d8ad4a003c15e6
MajinBuu <phucpro331@gmail.com>
2025-07-10 09:08:12 +0700

    f

--- RemoteMc.py
+++ RemoteMc.py
@@ -23,4 +23,5 @@ def status():
     return f"Polling is currently {'ENABLED' if poll_enabled else 'DISABLED'}."
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
